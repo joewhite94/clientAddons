052bd6fe9590d3676b738e386375dc2ba3aa3121
PiZZAD0X <509thparachuteinfantry@gmail.com>
2023-10-17 12:21:44 -0700

    formatting

--- Addons/website/gui/interruptMenu.hpp
+++ Addons/website/gui/interruptMenu.hpp
@@ -1,91 +1,91 @@
 class RscStandardDisplay;
 class RscDisplayMPInterrupt: RscStandardDisplay {
     class controls {
-        class gc_gcWebsiteFunctions_submit_bugreport_sendBoxTitle: gc_gcWebsiteFunctions_submit_bugreport_sendBoxTitleBase {};
-        class gc_gcWebsiteFunctions_submit_bugreport_sendBoxBG: gc_gcWebsiteFunctions_submit_bugreport_sendBoxBGBase {};
-        class gc_gcWebsiteFunctions_submit_bugreport_sendBox: gc_gcWebsiteFunctions_submit_bugreport_sendBoxBase {};
-        class gc_gcWebsiteFunctions_submit_bugreport_sendBoxButton: gc_gcWebsiteFunctions_submit_bugreport_sendBoxButtonBase {};
+        class GVAR(submit_bugreport_sendBoxTitle): GVAR(submit_bugreport_sendBoxTitleBase) {};
+        class GVAR(submit_bugreport_sendBoxBG): GVAR(submit_bugreport_sendBoxBGBase) {};
+        class GVAR(submit_bugreport_sendBox): GVAR(submit_bugreport_sendBoxBase) {};
+        class GVAR(submit_bugreport_sendBoxButton): GVAR(submit_bugreport_sendBoxButtonBase) {};
 
-        class gc_gcWebsiteFunctions_submit_review_sendBoxTitle: gc_gcWebsiteFunctions_submit_review_sendBoxTitleBase {};
-        class gc_gcWebsiteFunctions_submit_review_sendBoxBG: gc_gcWebsiteFunctions_submit_review_sendBoxBGBase {};
-        class gc_gcWebsiteFunctions_submit_review_sendBox: gc_gcWebsiteFunctions_submit_review_sendBoxBase {};
-        class gc_gcWebsiteFunctions_submit_review_sendBoxButton: gc_gcWebsiteFunctions_submit_review_sendBoxButtonBase {};
+        class GVAR(submit_review_sendBoxTitle): GVAR(submit_review_sendBoxTitleBase) {};
+        class GVAR(submit_review_sendBoxBG): GVAR(submit_review_sendBoxBGBase) {};
+        class GVAR(submit_review_sendBox): GVAR(submit_review_sendBoxBase) {};
+        class GVAR(submit_review_sendBoxButton): GVAR(submit_review_sendBoxButtonBase) {};
 
-        class gc_gcWebsiteFunctions_submit_rating_sendBoxTitle: gc_gcWebsiteFunctions_submit_rating_sendBoxTitleBase  {};
-        class gc_gcWebsiteFunctions_submit_rating_sendBoxListbox: gc_gcWebsiteFunctions_submit_rating_sendBoxListboxBase  {};
-        class gc_gcWebsiteFunctions_submit_rating_sendBoxButton : gc_gcWebsiteFunctions_submit_rating_sendBoxButtonBase  {};
+        class GVAR(submit_rating_sendBoxTitle): GVAR(submit_rating_sendBoxTitleBase)  {};
+        class GVAR(submit_rating_sendBoxListbox): GVAR(submit_rating_sendBoxListboxBase)  {};
+        class GVAR(submit_rating_sendBoxButton) : GVAR(submit_rating_sendBoxButtonBase)  {};
     };
 };
 
 class RscDisplayInterruptEditorPreview: RscStandardDisplay {
     class controls {
-        class gc_gcWebsiteFunctions_submit_bugreport_sendBoxTitle: gc_gcWebsiteFunctions_submit_bugreport_sendBoxTitleBase {};
-        class gc_gcWebsiteFunctions_submit_bugreport_sendBoxBG: gc_gcWebsiteFunctions_submit_bugreport_sendBoxBGBase {};
-        class gc_gcWebsiteFunctions_submit_bugreport_sendBox: gc_gcWebsiteFunctions_submit_bugreport_sendBoxBase {};
-        class gc_gcWebsiteFunctions_submit_bugreport_sendBoxButton: gc_gcWebsiteFunctions_submit_bugreport_sendBoxButtonBase {};
+        class GVAR(submit_bugreport_sendBoxTitle): GVAR(submit_bugreport_sendBoxTitleBase) {};
+        class GVAR(submit_bugreport_sendBoxBG): GVAR(submit_bugreport_sendBoxBGBase) {};
+        class GVAR(submit_bugreport_sendBox): GVAR(submit_bugreport_sendBoxBase) {};
+        class GVAR(submit_bugreport_sendBoxButton): GVAR(submit_bugreport_sendBoxButtonBase) {};
 
-        class gc_gcWebsiteFunctions_submit_review_sendBoxTitle: gc_gcWebsiteFunctions_submit_review_sendBoxTitleBase {};
-        class gc_gcWebsiteFunctions_submit_review_sendBoxBG: gc_gcWebsiteFunctions_submit_review_sendBoxBGBase {};
-        class gc_gcWebsiteFunctions_submit_review_sendBox: gc_gcWebsiteFunctions_submit_review_sendBoxBase {};
-        class gc_gcWebsiteFunctions_submit_review_sendBoxButton: gc_gcWebsiteFunctions_submit_review_sendBoxButtonBase {};
+        class GVAR(submit_review_sendBoxTitle): GVAR(submit_review_sendBoxTitleBase) {};
+        class GVAR(submit_review_sendBoxBG): GVAR(submit_review_sendBoxBGBase) {};
+        class GVAR(submit_review_sendBox): GVAR(submit_review_sendBoxBase) {};
+        class GVAR(submit_review_sendBoxButton): GVAR(submit_review_sendBoxButtonBase) {};
         
-        class gc_gcWebsiteFunctions_submit_rating_sendBoxTitle: gc_gcWebsiteFunctions_submit_rating_sendBoxTitleBase  {};
-        class gc_gcWebsiteFunctions_submit_rating_sendBoxListbox: gc_gcWebsiteFunctions_submit_rating_sendBoxListboxBase  {};
-        class gc_gcWebsiteFunctions_submit_rating_sendBoxButton : gc_gcWebsiteFunctions_submit_rating_sendBoxButtonBase  {};
+        class GVAR(submit_rating_sendBoxTitle): GVAR(submit_rating_sendBoxTitleBase)  {};
+        class GVAR(submit_rating_sendBoxListbox): GVAR(submit_rating_sendBoxListboxBase)  {};
+        class GVAR(submit_rating_sendBoxButton) : GVAR(submit_rating_sendBoxButtonBase)  {};
  
     };
 };
 
 class RscDisplayInterrupt: RscStandardDisplay {
     class controls {
-        class gc_gcWebsiteFunctions_submit_bugreport_sendBoxTitle: gc_gcWebsiteFunctions_submit_bugreport_sendBoxTitleBase {};
-        class gc_gcWebsiteFunctions_submit_bugreport_sendBoxBG: gc_gcWebsiteFunctions_submit_bugreport_sendBoxBGBase {};
-        class gc_gcWebsiteFunctions_submit_bugreport_sendBox: gc_gcWebsiteFunctions_submit_bugreport_sendBoxBase {};
-        class gc_gcWebsiteFunctions_submit_bugreport_sendBoxButton: gc_gcWebsiteFunctions_submit_bugreport_sendBoxButtonBase {};
+        class GVAR(submit_bugreport_sendBoxTitle): GVAR(submit_bugreport_sendBoxTitleBase) {};
+        class GVAR(submit_bugreport_sendBoxBG): GVAR(submit_bugreport_sendBoxBGBase) {};
+        class GVAR(submit_bugreport_sendBox): GVAR(submit_bugreport_sendBoxBase) {};
+        class GVAR(submit_bugreport_sendBoxButton): GVAR(submit_bugreport_sendBoxButtonBase) {};
 
-        class gc_gcWebsiteFunctions_submit_review_sendBoxTitle: gc_gcWebsiteFunctions_submit_review_sendBoxTitleBase {};
-        class gc_gcWebsiteFunctions_submit_review_sendBoxBG: gc_gcWebsiteFunctions_submit_review_sendBoxBGBase {};
-        class gc_gcWebsiteFunctions_submit_review_sendBox: gc_gcWebsiteFunctions_submit_review_sendBoxBase {};
-        class gc_gcWebsiteFunctions_submit_review_sendBoxButton: gc_gcWebsiteFunctions_submit_review_sendBoxButtonBase {};
+        class GVAR(submit_review_sendBoxTitle): GVAR(submit_review_sendBoxTitleBase) {};
+        class GVAR(submit_review_sendBoxBG): GVAR(submit_review_sendBoxBGBase) {};
+        class GVAR(submit_review_sendBox): GVAR(submit_review_sendBoxBase) {};
+        class GVAR(submit_review_sendBoxButton): GVAR(submit_review_sendBoxButtonBase) {};
 
-            class gc_gcWebsiteFunctions_submit_rating_sendBoxTitle: gc_gcWebsiteFunctions_submit_rating_sendBoxTitleBase  {};
-        class gc_gcWebsiteFunctions_submit_rating_sendBoxListbox: gc_gcWebsiteFunctions_submit_rating_sendBoxListboxBase  {};
-        class gc_gcWebsiteFunctions_submit_rating_sendBoxButton : gc_gcWebsiteFunctions_submit_rating_sendBoxButtonBase  {};
+            class GVAR(submit_rating_sendBoxTitle): GVAR(submit_rating_sendBoxTitleBase)  {};
+        class GVAR(submit_rating_sendBoxListbox): GVAR(submit_rating_sendBoxListboxBase)  {};
+        class GVAR(submit_rating_sendBoxButton) : GVAR(submit_rating_sendBoxButtonBase)  {};
     };
 };
 
 class RscDisplayInterruptEditor3D: RscStandardDisplay {
     class controls {
-        class gc_gcWebsiteFunctions_submit_bugreport_sendBoxTitle: gc_gcWebsiteFunctions_submit_bugreport_sendBoxTitleBase {};
-        class gc_gcWebsiteFunctions_submit_bugreport_sendBoxBG: gc_gcWebsiteFunctions_submit_bugreport_sendBoxBGBase {};
-        class gc_gcWebsiteFunctions_submit_bugreport_sendBox: gc_gcWebsiteFunctions_submit_bugreport_sendBoxBase {};
-        class gc_gcWebsiteFunctions_submit_bugreport_sendBoxButton: gc_gcWebsiteFunctions_submit_bugreport_sendBoxButtonBase {};
+        class GVAR(submit_bugreport_sendBoxTitle): GVAR(submit_bugreport_sendBoxTitleBase) {};
+        class GVAR(submit_bugreport_sendBoxBG): GVAR(submit_bugreport_sendBoxBGBase) {};
+        class GVAR(submit_bugreport_sendBox): GVAR(submit_bugreport_sendBoxBase) {};
+        class GVAR(submit_bugreport_sendBoxButton): GVAR(submit_bugreport_sendBoxButtonBase) {};
 
-        class gc_gcWebsiteFunctions_submit_review_sendBoxTitle: gc_gcWebsiteFunctions_submit_review_sendBoxTitleBase {};
-        class gc_gcWebsiteFunctions_submit_review_sendBoxBG: gc_gcWebsiteFunctions_submit_review_sendBoxBGBase {};
-        class gc_gcWebsiteFunctions_submit_review_sendBox: gc_gcWebsiteFunctions_submit_review_sendBoxBase {};
-        class gc_gcWebsiteFunctions_submit_review_sendBoxButton: gc_gcWebsiteFunctions_submit_review_sendBoxButtonBase {};
+        class GVAR(submit_review_sendBoxTitle): GVAR(submit_review_sendBoxTitleBase) {};
+        class GVAR(submit_review_sendBoxBG): GVAR(submit_review_sendBoxBGBase) {};
+        class GVAR(submit_review_sendBox): GVAR(submit_review_sendBoxBase) {};
+        class GVAR(submit_review_sendBoxButton): GVAR(submit_review_sendBoxButtonBase) {};
         
-        class gc_gcWebsiteFunctions_submit_rating_sendBoxTitle: gc_gcWebsiteFunctions_submit_rating_sendBoxTitleBase  {};
-        class gc_gcWebsiteFunctions_submit_rating_sendBoxListbox: gc_gcWebsiteFunctions_submit_rating_sendBoxListboxBase  {};
-        class gc_gcWebsiteFunctions_submit_rating_sendBoxButton : gc_gcWebsiteFunctions_submit_rating_sendBoxButtonBase  {};
+        class GVAR(submit_rating_sendBoxTitle): GVAR(submit_rating_sendBoxTitleBase)  {};
+        class GVAR(submit_rating_sendBoxListbox): GVAR(submit_rating_sendBoxListboxBase)  {};
+        class GVAR(submit_rating_sendBoxButton) : GVAR(submit_rating_sendBoxButtonBase)  {};
     };
 };
 
 class RscDisplayMovieInterrupt: RscStandardDisplay {
     class controls {
-        class gc_gcWebsiteFunctions_submit_bugreport_sendBoxTitle: gc_gcWebsiteFunctions_submit_bugreport_sendBoxTitleBase {};
-        class gc_gcWebsiteFunctions_submit_bugreport_sendBoxBG: gc_gcWebsiteFunctions_submit_bugreport_sendBoxBGBase {};
-        class gc_gcWebsiteFunctions_submit_bugreport_sendBox: gc_gcWebsiteFunctions_submit_bugreport_sendBoxBase {};
-        class gc_gcWebsiteFunctions_submit_bugreport_sendBoxButton: gc_gcWebsiteFunctions_submit_bugreport_sendBoxButtonBase {};
+        class GVAR(submit_bugreport_sendBoxTitle): GVAR(submit_bugreport_sendBoxTitleBase) {};
+        class GVAR(submit_bugreport_sendBoxBG): GVAR(submit_bugreport_sendBoxBGBase) {};
+        class GVAR(submit_bugreport_sendBox): GVAR(submit_bugreport_sendBoxBase) {};
+        class GVAR(submit_bugreport_sendBoxButton): GVAR(submit_bugreport_sendBoxButtonBase) {};
 
-        class gc_gcWebsiteFunctions_submit_review_sendBoxTitle: gc_gcWebsiteFunctions_submit_review_sendBoxTitleBase {};
-        class gc_gcWebsiteFunctions_submit_review_sendBoxBG: gc_gcWebsiteFunctions_submit_review_sendBoxBGBase {};
-        class gc_gcWebsiteFunctions_submit_review_sendBox: gc_gcWebsiteFunctions_submit_review_sendBoxBase {};
-        class gc_gcWebsiteFunctions_submit_review_sendBoxButton: gc_gcWebsiteFunctions_submit_review_sendBoxButtonBase {};
+        class GVAR(submit_review_sendBoxTitle): GVAR(submit_review_sendBoxTitleBase) {};
+        class GVAR(submit_review_sendBoxBG): GVAR(submit_review_sendBoxBGBase) {};
+        class GVAR(submit_review_sendBox): GVAR(submit_review_sendBoxBase) {};
+        class GVAR(submit_review_sendBoxButton): GVAR(submit_review_sendBoxButtonBase) {};
 
-        class gc_gcWebsiteFunctions_submit_rating_sendBoxTitle: gc_gcWebsiteFunctions_submit_rating_sendBoxTitleBase  {};
-        class gc_gcWebsiteFunctions_submit_rating_sendBoxListbox: gc_gcWebsiteFunctions_submit_rating_sendBoxListboxBase  {};
-        class gc_gcWebsiteFunctions_submit_rating_sendBoxButton : gc_gcWebsiteFunctions_submit_rating_sendBoxButtonBase  {};
+        class GVAR(submit_rating_sendBoxTitle): GVAR(submit_rating_sendBoxTitleBase)  {};
+        class GVAR(submit_rating_sendBoxListbox): GVAR(submit_rating_sendBoxListboxBase)  {};
+        class GVAR(submit_rating_sendBoxButton) : GVAR(submit_rating_sendBoxButtonBase)  {};
     };
 };
